96c7e0885b65c988acb8dfc112d2d39c6520a85a
adriano terrazas <adrianoterrazas2019@gmail.com>
2026-05-22 16:22:25 +0200

    Rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles, except: :index
+
   resources :lists, only: [ :index, :show, :new, :create ] do
    resources :bookmarks, only: [ :new, :create ]
   end
